d802b2add53709acbd74c0f55a61ac4a724930a7
HusaiGG <45373923+HusaiGG@users.noreply.github.com>
2022-02-21 09:55:49 -0600

    Kata1

---
+++ Kata1-Doc.py
@@ -0,0 +1,16 @@
+from datetime import date
+
+date.today()
+
+print(date.today())
+
+print("Today's date is: " + str(date.today()))
+
+print("Bienvenido al progrmama de bienvenida")
+name = input("Introduzca su nombre ")
+print("Saludos: " + name)
+
+print("Calculadora")
+first_number = input("Primer numero: ")
+second_number = input("Segundo número: ")
+print(int(first_number) + int(second_number))
